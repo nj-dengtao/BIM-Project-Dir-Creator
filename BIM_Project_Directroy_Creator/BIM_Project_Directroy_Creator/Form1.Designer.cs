8c631ecf8fadd340753cb0c1e3dfa9fb7cf7dec7
DengTao <njdengtao@outlook.com>
2018-12-24 11:30:33 +0800

    1-test

--- BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.Designer.cs
+++ BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.Designer.cs
@@ -28,20 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lab_ToolName = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lab_ToolName
+            // 
+            this.lab_ToolName.AutoSize = true;
+            this.lab_ToolName.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_ToolName.Location = new System.Drawing.Point(282, 11);
+            this.lab_ToolName.Name = "lab_ToolName";
+            this.lab_ToolName.Size = new System.Drawing.Size(234, 22);
+            this.lab_ToolName.TabIndex = 0;
+            this.lab_ToolName.Text = "BIM项目标准目录结构创建工具";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lab_ToolName);
             this.Name = "Form1";
             this.Text = "CCIEE-BIM Project Directory Creator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lab_ToolName;
     }
 }
 
