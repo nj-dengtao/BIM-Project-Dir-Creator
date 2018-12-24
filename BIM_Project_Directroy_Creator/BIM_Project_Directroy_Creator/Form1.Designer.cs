d942015e73a50772d46ffb2f7c67b29c9dc6c1fc
DengTao <njdengtao@outlook.com>
2018-12-24 17:29:15 +0800

    2018-12-24:基本框架完成

--- BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.Designer.cs
+++ BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.Designer.cs
@@ -28,7 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Root");
             this.lab_ToolName = new System.Windows.Forms.Label();
+            this.tre_Driectroy = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.rad_FromTemplate = new System.Windows.Forms.RadioButton();
+            this.rad_Manual = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lab_TemplateFile = new System.Windows.Forms.Label();
+            this.but_SelectTemplate = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lab_ToolName
@@ -41,14 +53,135 @@
             this.lab_ToolName.TabIndex = 0;
             this.lab_ToolName.Text = "BIM项目标准目录结构创建工具";
             // 
+            // tre_Driectroy
+            // 
+            this.tre_Driectroy.Font = new System.Drawing.Font("微软雅黑", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tre_Driectroy.Location = new System.Drawing.Point(12, 57);
+            this.tre_Driectroy.Name = "tre_Driectroy";
+            treeNode2.Name = "节点0";
+            treeNode2.Text = "Root";
+            this.tre_Driectroy.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.tre_Driectroy.Size = new System.Drawing.Size(311, 529);
+            this.tre_Driectroy.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Child Node";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(339, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add Parent Node";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(339, 384);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Delete Node";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // rad_FromTemplate
+            // 
+            this.rad_FromTemplate.AutoSize = true;
+            this.rad_FromTemplate.Checked = true;
+            this.rad_FromTemplate.Location = new System.Drawing.Point(23, 42);
+            this.rad_FromTemplate.Name = "rad_FromTemplate";
+            this.rad_FromTemplate.Size = new System.Drawing.Size(109, 17);
+            this.rad_FromTemplate.TabIndex = 5;
+            this.rad_FromTemplate.TabStop = true;
+            this.rad_FromTemplate.Text = "从模板文件创建";
+            this.rad_FromTemplate.UseVisualStyleBackColor = true;
+            this.rad_FromTemplate.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rad_Manual
+            // 
+            this.rad_Manual.AutoSize = true;
+            this.rad_Manual.Location = new System.Drawing.Point(23, 19);
+            this.rad_Manual.Name = "rad_Manual";
+            this.rad_Manual.Size = new System.Drawing.Size(73, 17);
+            this.rad_Manual.TabIndex = 6;
+            this.rad_Manual.Text = "手动创建";
+            this.rad_Manual.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lab_TemplateFile);
+            this.groupBox1.Controls.Add(this.but_SelectTemplate);
+            this.groupBox1.Controls.Add(this.rad_Manual);
+            this.groupBox1.Controls.Add(this.rad_FromTemplate);
+            this.groupBox1.Location = new System.Drawing.Point(329, 57);
+            this.groupBox1.MaximumSize = new System.Drawing.Size(500, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 100);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "创建模式";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // lab_TemplateFile
+            // 
+            this.lab_TemplateFile.AutoSize = true;
+            this.lab_TemplateFile.Enabled = false;
+            this.lab_TemplateFile.Location = new System.Drawing.Point(7, 79);
+            this.lab_TemplateFile.Name = "lab_TemplateFile";
+            this.lab_TemplateFile.Size = new System.Drawing.Size(115, 13);
+            this.lab_TemplateFile.TabIndex = 8;
+            this.lab_TemplateFile.Text = "请选择一个模板文件";
+            // 
+            // but_SelectTemplate
+            // 
+            this.but_SelectTemplate.Location = new System.Drawing.Point(138, 39);
+            this.but_SelectTemplate.Name = "but_SelectTemplate";
+            this.but_SelectTemplate.Size = new System.Drawing.Size(28, 23);
+            this.but_SelectTemplate.TabIndex = 7;
+            this.but_SelectTemplate.Text = "...";
+            this.but_SelectTemplate.UseVisualStyleBackColor = true;
+            this.but_SelectTemplate.Click += new System.EventHandler(this.but_SelectTemplate_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(339, 175);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "创建项目";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 598);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tre_Driectroy);
             this.Controls.Add(this.lab_ToolName);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "CCIEE-BIM Project Directory Creator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +190,16 @@
         #endregion
 
         private System.Windows.Forms.Label lab_ToolName;
+        private System.Windows.Forms.TreeView tre_Driectroy;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton rad_FromTemplate;
+        private System.Windows.Forms.RadioButton rad_Manual;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button but_SelectTemplate;
+        private System.Windows.Forms.Label lab_TemplateFile;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.cs
+++ BIM_Project_Directroy_Creator/BIM_Project_Directroy_Creator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace BIM_Project_Directroy_Creator
 {
@@ -15,6 +16,106 @@ namespace BIM_Project_Directroy_Creator
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(tre_Driectroy.SelectedNode == null)
+            {
+                MessageBox.Show("选择一个节点", "提示信息");
+            }
+            else
+            {
+                TreeNode tmp;
+                tmp = new TreeNode("节点");
+                tre_Driectroy.SelectedNode.Nodes.Add(tmp);
+                tre_Driectroy.SelectedNode = tmp;
+                tre_Driectroy.ExpandAll();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(tre_Driectroy.SelectedNode == null)
+            {
+                MessageBox.Show("请选择一个节点", "提示信息");
+            }
+            else
+            {
+                TreeNode tmp = new TreeNode("节点");
+                tre_Driectroy.SelectedNode.Parent.Nodes.Add(tmp);
+                tre_Driectroy.ExpandAll();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+          if (tre_Driectroy.SelectedNode.Nodes.Count == 0)
+            {
+                tre_Driectroy.SelectedNode.Remove();
+            }
+            else
+            {
+                MessageBox.Show("请先删除此节点下的所有子节点", "提示信息", MessageBoxButtons.OK); 
+
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (this.rad_FromTemplate.Checked == true)
+            {
+                this.but_SelectTemplate.Enabled = true;
+            }
+            else
+            {
+                this.but_SelectTemplate.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.groupBox1.Width = 250;
+        }
+
+        private void but_SelectTemplate_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = @"d:\";
+            openFileDialog1.Filter = "Text File|*.txt";
+            openFileDialog1.RestoreDirectory = true;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                lab_TemplateFile.Text = openFileDialog1.FileName;
+                this.groupBox1.Width = this.lab_TemplateFile.Width +50;
+            }
+            else
+            {
+                lab_TemplateFile.Text = "请选择一个模板文件";
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(lab_TemplateFile.Text);
+                using (sr) ;
+                {
+                    string ProjectName = sr.ReadLine().Trim();
+                    this.tre_Driectroy.Nodes.Add(ProjectName);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
